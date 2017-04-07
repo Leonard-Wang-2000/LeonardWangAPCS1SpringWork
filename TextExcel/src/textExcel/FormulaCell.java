3dbaf3821e62274b7034fb138d72b38b5f6b30cd
Leonard Wang <18wangl64@millshighsch.org>
2017-04-07 13:09:04 -0700

    asdadasdasd

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -8,19 +8,24 @@ public class FormulaCell extends RealCell{
 		getDoubleValue();
 	}
 	public double getDoubleValue(){
-		String[] changeValue = value.split(" ");
-		double finalValue = Double.parseDouble(changeValue[1]);
+		String noParenthesis = value.substring(2, value.length() - 2);
+		String[] changeValue = noParenthesis.split(" ");
+		if(changeValue.length == 1){
+			return Double.parseDouble(changeValue[0]);
+		}
+		for(int i = 0;)
+		double finalValue = Double.parseDouble(changeValue[0]);
 		//( 5.4 * 3.5 / -1.4 + 27.4 - 11.182 ) 
 		//increments by 2 and tests for signs to correctly 
-		for(int i = 4; i < changeValue.length; i+=2){
-			if(changeValue[i-2].equals("+")){
-				finalValue += Double.parseDouble(changeValue[i-1]);
-			} else if(changeValue[i-2].equals("*")){
-				finalValue *= Double.parseDouble(changeValue[i-1]);
-			} else if(changeValue[i-2].equals("/")){
-				finalValue /= Double.parseDouble(changeValue[i-1]);
+		for(int i = 2; i < changeValue.length; i+=2){
+			if(changeValue[i-1].equals("+")){
+				finalValue += Double.parseDouble(changeValue[i]);
+			} else if(changeValue[i-1].equals("*")){
+				finalValue *= Double.parseDouble(changeValue[i]);
+			} else if(changeValue[i-1].equals("/")){
+				finalValue /= Double.parseDouble(changeValue[i]);
 			} else{
-				finalValue -= Double.parseDouble(changeValue[i-1]);
+				finalValue -= Double.parseDouble(changeValue[i]);
 			}
 		}
 		setRealCell(finalValue + "");
@@ -29,4 +34,5 @@ public class FormulaCell extends RealCell{
 	public String fullCellText(){
 		return value;
 	}
+	public 
 }
