b436c4f2e285ee0ce5897af58be66fbc4bfdec51
LeonardWang2000 <18wangl64@millshighsch.org>
2017-04-17 08:54:00 -0700

    testshit

---
+++ testStuff/src/testStuff/testing.java
@@ -0,0 +1,15 @@
+package testStuff;
+
+public class testing {
+
+	public static void main(String[] args) {
+		int a = 12;
+		double b = 2.5;
+		int c = 5;
+		double d = (double)(a/c);
+		System.out.println(d);
+		System.out.println((double)(a/c));
+
+	}
+
+}
